d8bdc0d39e56a9a5fc430e4699a7dc2ebe54cbeb
Alberto <unumnio@gmail.com>
2023-08-28 16:45:27 -0500

    implement Ui to global panel of Scena StartMenu

---
+++ Assets/Scripts/StartMenuScripts/PanelUi.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using TMPro.EditorUtilities;
+
+public class PanelUi : MonoBehaviour
+{
+    public TextMeshProUGUI bestScore;
+    public TMP_InputFieldEditor namePlayer;
+    public Button start;
+    public Button quit;
+}
