1bd1e30d8d49e6c2591715e436c77fd1b6895c21
Alberto <unumnio@gmail.com>
2023-08-27 23:31:41 -0500

    Creating Scoreboard in the startMenu

---
+++ Assets/Scripts/ScoreUI.cs
@@ -0,0 +1,22 @@
+using System.Linq;
+using UnityEngine;
+
+public class ScoreUI : MonoBehaviour
+{
+    public RowUI rowUi;
+    public ScoreManager scoreManager;
+    void Start()
+    {
+        scoreManager.AddScore(new Score("sub",9999));
+
+        var scores = scoreManager.GetHighScores().ToArray();
+        for (int i = 0; i < scores.Length; i++)
+        {
+            var row = Instantiate(rowUi, transform).GetComponent<RowUI>();
+            row.rank.text = (i + 1).ToString();
+            row.name.text = scores[i].name;
+            row.score.text = scores[i].score.ToString();
+        }
+    }
+
+}
